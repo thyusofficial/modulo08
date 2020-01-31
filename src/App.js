ab083a745f1b44ea9be84ef0ab17bc60b0433261
Matheus Cardoso <T-TI-07@Dev03.gestaotera.local>
2020-01-31 10:58:40 -0300

    useEffect, Memo & Callback

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
-  const [tech, setTech] = useState([
-    'ReactJS',
-    'React Native'
-  ]);
-  const [newTech, setNewTech] = useState('')
-  function handleAdd() {
-    setTech([...tech, newTech])
-    setNewTech('')
-  }
+  const [tech, setTech] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  const handleAdd = useCallback(() => {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }, [newTech, tech]);
+
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
+  const techSize = useMemo(() => tech.length, [tech]);
+
   return (
     <>
       <ul>
@@ -17,6 +29,8 @@ function App() {
           <li key={t}>{t}</li>
         ))}
       </ul>
+      <strong>Você tem {techSize} Tecnologias</strong>
+      <br />
       <input value={newTech} onChange={e => setNewTech(e.target.value)} />
       <button type="button" onClick={handleAdd}>
         Adicionar
